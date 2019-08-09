035b4a1beb3a183ba487181a976e9e6806fd6046
carlosmarinho <carluizfla@hotmail.com>
2019-08-08 23:44:35 -0300

    feito o search do searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-    onInputChange(event) {
+    /* onInputChange(event) {
         //event.target.value
+    } */
+
+    state = { term: '' };
+
+    /* If we do this, it will automatically bind the 'this */
+    onFormSubmit = (event) => {
+        event.preventDefault();
+
+        this.props.onSubmit(this.state.term);
     }
+    
+    /* if we do this we have to bind the this on constructor or on the creting function
+     * <form onSubmit={event => this.onFormSubmit(event)}
+    onFormSubmit(event) {
+        event.preventDefault();
+    }
+    */
 
     render(){
         return(
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} /*onSubmit={event => this.onFormSubmit(event)}*/ className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" onChange={this.onInputChange} /*onChange={(e) => {e.target.value}}*/ />
+                        <input type="text" /*onChange={this.onInputChange} */ /*onChange={(e) => {e.target.value}}*/ 
+                            value={this.state.term}
+                            onChange={e => this.setState({ term: e.target.value })}
+                        />
                     </div>
                 </form>
             </div>
